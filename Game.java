7e7765b508460e425eddb257810da4bdd3ba3d7c
AmirHossein <amirhosseinamadeh@gmail.com>
2022-03-19 10:04:26 +0330

    Singleton_PlayersList

--- Game.java
+++ Game.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Game{
     private static Game game = new Game();
+    private Player[] players = new Player[4];
+    Scanner scanner = new Scanner(System.in);
 
     private Game() {
     }
     public static Game create_game(){
         return game;
+    }
+    public void StartGame() {
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new Player(scanner.nextLine(), i+1);
+        }
     }
     
 }
