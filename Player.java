83569cdab95537639d81db49c492134e48fa6fb5
AmirHossein <amirhosseinamadeh@gmail.com>
2022-03-19 10:06:29 +0330

    GenerateConstructor

--- Player.java
+++ Player.java
@@ -1,3 +1,10 @@
 public class Player {
+    private String name;
+    private int Turn;
+    public Player(String name, int turn) {
+        this.name = name;
+        Turn = turn;
+    }
+    
     
 }
