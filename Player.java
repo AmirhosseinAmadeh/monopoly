3522bbda26517e3a19785bea7c1fb19fbc27387f
AmirHossein <amirhosseinamadeh@gmail.com>
2022-03-29 16:16:26 +0430

    MonopolyAdded

--- Player.java
+++ Player.java
@@ -3,6 +3,10 @@ public class Player {
     private int Turn;
     private double CashAssets = 1500;
     private int[] Non_cashAssets = new int[12];
+    boolean RedMonopoly = false;
+    boolean BlueMonopoly = false;
+    boolean GreenMonopoly = false;
+    boolean YeloowMonopoly = false;
     public Player(String name, int turn) {
         this.name = name;
         Turn = turn;
