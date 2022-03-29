9b308e4d3c1d9472dd2cb10d4af2ab7f4fe1117b
AmirHossein <amirhosseinamadeh@gmail.com>
2022-03-29 14:20:27 +0430

    HomeClassAdded

---
+++ Home.java
@@ -0,0 +1,15 @@
+public abstract class Home {
+    private int woner = 0;
+
+    public Home() {
+    }
+
+    public int getWoner() {
+        return woner;
+    }
+
+    public void setWoner(int woner) {
+        this.woner = woner;
+    }
+    
+}
