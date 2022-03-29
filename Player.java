5a46c03f54f3fd75db78eea7e6cdc086b697a180
AmirHossein <amirhosseinamadeh@gmail.com>
2022-03-29 14:32:57 +0430

    Added assets to player class

--- Player.java
+++ Player.java
@@ -1,6 +1,8 @@
 public class Player {
     private String name;
     private int Turn;
+    private double CashAssets = 1500;
+    private int[] Non_cashAssets = new int[12];
     public Player(String name, int turn) {
         this.name = name;
         Turn = turn;
